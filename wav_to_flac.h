a42dcadb8b692a61545a233c72be866dc4b612e9
Jason Hurt <jleehurt@gmail.com>
2012-07-08 13:16:18 -0700

    initial commit

---
+++ wav_to_flac.h
@@ -0,0 +1,14 @@
+//
+//  wav_to_flac.h
+//
+//  Created by Jason Hurt on 7/6/12.
+#ifdef __cplusplus
+extern "C"
+#endif
+
+#ifndef Lucidity_wav_to_flac_h
+#define Lucidity_wav_to_flac_h
+
+int convertWavToFlac(const char *wave_file_in, const char *flac_file_out);
+
+#endif
