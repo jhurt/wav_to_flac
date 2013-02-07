0cedba24d8025d2a1078ea8807540fe989b4f83f
Jason Hurt <jleehurt@gmail.com>
2013-02-06 19:15:54 -0800

    add option to encode multiple output flac files as specified by an interval in seconds. less error checking.f

--- wav_to_flac.c
+++ wav_to_flac.c
@@ -1,5 +1,5 @@
 /*
- * Use libFLAC to convert an Apple WAVE file to a flac encoded file
+ * Use libFLAC to convert an Apple WAVE file to one or more flac encoded files
  * Created by Jason Hurt on 7/6/12.
  */
 
@@ -20,26 +20,26 @@ static void progress_callback(const FLAC__StreamEncoder *encoder, FLAC__uint64 b
 static FLAC__byte buffer[READSIZE/*samples*/ * 2/*bytes_per_sample*/ * 2/*channels*/];
 static FLAC__int32 pcm[READSIZE/*samples*/ * 2/*channels*/];
 
-int convertWavToFlac(const char *wave_file_in, const char *flac_file_out) {
-	FILE *fin;
-	if((fin = fopen(wave_file_in, "rb")) == NULL) {
-		fprintf(stderr, "ERROR: opening %s for output\n", wave_file_in);
-		return 1;
-	}
+int convertWavToFlac(const char *wave_file, const char *flac_file, int split_interval_seconds, char** out_flac_files) {
+    FILE *fin;
+    if((fin = fopen(wave_file, "rb")) == NULL) {
+        fprintf(stderr, "ERROR: opening %s for output\n", wave_file);
+        return 1;
+    }
     
-	// read wav header and validate it, note this will most likely fail for WAVE files not created by Apple
-	if(fread(buffer, 1, 44, fin) != 44 ||
+    // read wav header and validate it, note this will most likely fail for WAVE files not created by Apple
+    if(fread(buffer, 1, 44, fin) != 44 ||
        memcmp(buffer, "RIFF", 4) ||
        memcmp(buffer+36, "FLLR", 4)) {
-		fprintf(stderr, "ERROR: invalid/unsupported WAVE file\n");
-		fclose(fin);
-		return 1;
-	}
+        fprintf(stderr, "ERROR: invalid/unsupported WAVE file\n");
+        fclose(fin);
+        return 1;
+    }
     unsigned num_channels = ((unsigned)buffer[23] << 8) | buffer[22];;
-	unsigned sample_rate = ((((((unsigned)buffer[27] << 8) | buffer[26]) << 8) | buffer[25]) << 8) | buffer[24];
+    unsigned sample_rate = ((((((unsigned)buffer[27] << 8) | buffer[26]) << 8) | buffer[25]) << 8) | buffer[24];
     //unsigned byte_rate = ((((((unsigned)buffer[31] << 8) | buffer[30]) << 8) | buffer[29]) << 8) | buffer[28];
     //unsigned block_align = ((unsigned)buffer[33] << 8) | buffer[32];
-	unsigned bps = ((unsigned)buffer[35] << 8) | buffer[34];
+    unsigned bps = ((unsigned)buffer[35] << 8) | buffer[34];
     
     //Apple puts the number of filler bytes in the 2 bytes following FLLR in the filler chunk
     //get the int value of the hex
@@ -59,61 +59,89 @@ int convertWavToFlac(const char *wave_file_in, const char *flac_file_out) {
         //Subchunk2Size == NumSamples * NumChannels * BitsPerSample/8
         data_subchunk_size = ((((((unsigned)buffer[7] << 8) | buffer[6]) << 8) | buffer[5]) << 8) | buffer[4];
     }
-        
-	FLAC__StreamEncoder *encoder = 0;
-	if((encoder = FLAC__stream_encoder_new()) == NULL) {
-		fprintf(stderr, "ERROR: allocating encoder\n");
-		fclose(fin);
-		return 1;
-	}
-    
-    FLAC__bool ok = true;
-	ok &= FLAC__stream_encoder_set_verify(encoder, true);
-	ok &= FLAC__stream_encoder_set_compression_level(encoder, 5);
-	ok &= FLAC__stream_encoder_set_channels(encoder, num_channels);
-	ok &= FLAC__stream_encoder_set_bits_per_sample(encoder, bps);
-	ok &= FLAC__stream_encoder_set_sample_rate(encoder, sample_rate);
-    //unknown total samples
-	ok &= FLAC__stream_encoder_set_total_samples_estimate(encoder, 0);
-    
+
     //create the flac encoder
-	if(ok) {
-		FLAC__StreamEncoderInitStatus init_status = FLAC__stream_encoder_init_file(encoder, flac_file_out, progress_callback, /*client_data=*/NULL);
-		if(init_status != FLAC__STREAM_ENCODER_INIT_STATUS_OK) {
-			fprintf(stderr, "ERROR: initializing encoder: %s\n", FLAC__StreamEncoderInitStatusString[init_status]);
-			ok = false;
-		}
-	}
+    FLAC__StreamEncoder *encoder = FLAC__stream_encoder_new();
+    FLAC__stream_encoder_set_verify(encoder, true);
+    FLAC__stream_encoder_set_compression_level(encoder, 5);
+    FLAC__stream_encoder_set_channels(encoder, num_channels);
+    FLAC__stream_encoder_set_bits_per_sample(encoder, bps);
+    FLAC__stream_encoder_set_sample_rate(encoder, sample_rate);
+    //unknown total samples
+    FLAC__stream_encoder_set_total_samples_estimate(encoder, 0);
+    char* next_flac_file = malloc(sizeof(char) * 1024);
+    sprintf(next_flac_file, "%s.flac", flac_file);
+    fprintf(stderr, "writing to new flac file %s\n", next_flac_file);
+    FLAC__stream_encoder_init_file(encoder, next_flac_file, progress_callback, NULL);
     
+    long total_bytes_read = 0;
+    int did_split_at_interval[1024];
+    for(int i = 0; i < 1024; i++) {
+        did_split_at_interval[i] = 0;
+    }
+
     //read the wav file data chunk until we reach the end of the file.
-    if(ok) {
-        size_t bytes_read = 0;
-        size_t need = (size_t)READSIZE;
-		while((bytes_read = fread(buffer, num_channels * (bps/8), need, fin)) != 0) {
-            /* convert the packed little-endian 16-bit PCM samples from WAVE into an interleaved FLAC__int32 buffer for libFLAC */
-            size_t i;
-            for(i = 0; i < bytes_read*num_channels; i++) {
-                /* inefficient but simple and works on big- or little-endian machines */
-                pcm[i] = (FLAC__int32)(((FLAC__int16)(FLAC__int8)buffer[2*i+1] << 8) | (FLAC__int16)buffer[2*i]);
+    size_t bytes_read = 0;
+    size_t need = (size_t)READSIZE;
+    int flac_file_index = 0;
+    while((bytes_read = fread(buffer, num_channels * (bps/8), need, fin)) != 0) {
+        /* convert the packed little-endian 16-bit PCM samples from WAVE into an interleaved FLAC__int32 buffer for libFLAC */
+        size_t i;
+        for(i = 0; i < bytes_read*num_channels; i++) {
+            /* inefficient but simple and works on big- or little-endian machines */
+            pcm[i] = (FLAC__int32)(((FLAC__int16)(FLAC__int8)buffer[2*i+1] << 8) | (FLAC__int16)buffer[2*i]);
+        }
+        /* feed samples to encoder */
+        FLAC__stream_encoder_process_interleaved(encoder, pcm, bytes_read);
+        total_bytes_read += bytes_read;
+        
+        if(split_interval_seconds > 0) {
+            double elapsed_time_seconds = (total_bytes_read * 16) / (bps * sample_rate);
+            int interval = elapsed_time_seconds / split_interval_seconds;
+            if(interval > 0) {
+                if(!did_split_at_interval[interval-1]) {
+                    //finish encoding the current flac file
+                    FLAC__stream_encoder_finish(encoder);
+                    FLAC__stream_encoder_delete(encoder);
+                    
+                    //add the flac file to the out_flac_files output parameter
+                    *(out_flac_files + flac_file_index) = next_flac_file;
+                    flac_file_index += 1;
+                    
+                    //get a new flac file name
+                    //free(next_flac_file);
+                    next_flac_file = malloc(sizeof(char) * 1024);
+                    sprintf(next_flac_file, "%s_%d.flac", flac_file, interval);
+                    fprintf(stderr, "writing to new flac file %s\n", next_flac_file);
+                    
+                    //create a new encoder
+                    encoder = FLAC__stream_encoder_new();
+                    FLAC__stream_encoder_set_verify(encoder, true);
+                    FLAC__stream_encoder_set_compression_level(encoder, 5);
+                    FLAC__stream_encoder_set_channels(encoder, num_channels);
+                    FLAC__stream_encoder_set_bits_per_sample(encoder, bps);
+                    FLAC__stream_encoder_set_sample_rate(encoder, sample_rate);
+                    FLAC__stream_encoder_set_total_samples_estimate(encoder, 0);
+                    FLAC__stream_encoder_init_file(encoder, next_flac_file, progress_callback, NULL);
+                    
+                    //mark the interval as split
+                    did_split_at_interval[interval-1] = 1;
+                }
             }
-            /* feed samples to encoder */
-            ok = FLAC__stream_encoder_process_interleaved(encoder, pcm, bytes_read);
-		}
-	}
-    
-	ok &= FLAC__stream_encoder_finish(encoder);
-	fprintf(stderr, "encoding: %s\n", ok? "Conversion finished" : "Conversion failed");
+        }
+    }
+    fprintf(stderr, "total bytes read: %ld\nbits per sample: %d\nsample rate: %d\n", total_bytes_read, bps, sample_rate);
+
+    *(out_flac_files + flac_file_index) = next_flac_file;
 
     //cleanup
-	FLAC__stream_encoder_delete(encoder);
-	fclose(fin);
+    FLAC__stream_encoder_finish(encoder);
+    FLAC__stream_encoder_delete(encoder);
+    fclose(fin);
     
-    if(ok) {
-        return 0;
-    }
-    return 1;
+    return 0;
 }
 
 void progress_callback(const FLAC__StreamEncoder *encoder, FLAC__uint64 bytes_written, FLAC__uint64 samples_written, unsigned frames_written, unsigned total_frames_estimate, void *client_data) {
-	//fprintf(stderr, "wrote %llu bytes\n", bytes_written);
+    //fprintf(stderr, "wrote %llu bytes\n", bytes_written);
 }
--- wav_to_flac.h
+++ wav_to_flac.h
@@ -9,6 +9,13 @@ extern "C"
 #ifndef Lucidity_wav_to_flac_h
 #define Lucidity_wav_to_flac_h
 
-int convertWavToFlac(const char *wave_file_in, const char *flac_file_out);
+/**
+ ** wave_file: the full path and filename of the input wave file
+ ** flac_file: the full path and filename of the output flac file, without the .flac extension
+ ** split_interval_seconds: if > 0, there will be multiple flac files created every split_interval_seconds, 
+      so a 30 second file with split_interval_seconds == 10 will have 3 output files. Pass 0 to encode 1 output file.
+ ** out_flac_files: a list of char* to the output files created, should have enough space up to a max of 1024 output files
+ **/
+int convertWavToFlac(const char *wave_file_in, const char *flac_file_out, int split_interval_seconds, char** out_flac_files);
 
 #endif
